cad73a0bc5b488ef12ff4e4779a49dc342216585
ttaerrin <redgreenyell@naver.com>
2025-02-06 22:09:12 +0900

    02.06 like 기능 프론트

--- demos/models.py
+++ demos/models.py
@@ -29,6 +29,7 @@ class ParkingLot(models.Model):
     disabled_parking = models.BooleanField(default=False)  # 장애인 전용 주차 여부
     average_rating = models.FloatField(null=True, default=None) # 평균 평점
 
+
     def __str__(self):
         return f"{self.name} - {self.road_address}"
 
@@ -81,3 +82,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content
+    
